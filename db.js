4233557d0b2c0ed49e2f247b2749688e15167f2e
siddhi-khanvilkar <siddhikhanvilkar81@gmail.com>
2025-09-20 21:52:31 +0530

    Updated all

---
+++ db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+const promisePool = pool.promise();
+
+module.exports = promisePool;
