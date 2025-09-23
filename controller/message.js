517e2709cf5ae72a28b7faf8cc23af49d03b84b8
siddhi-khanvilkar <siddhikhanvilkar81@gmail.com>
2025-09-23 15:51:23 +0530

    Updated all

--- controller/message.js
+++ controller/message.js
@@ -1,41 +1,16 @@
-// controller/message.js
 const db = require('../db');
 
-exports.sendMessage = async (req, res) => {
+exports.sendMessage = (req, res) => {
   const { parentName, studentName, message } = req.body;
-  console.log('sendMessage: received form data ->', { parentName, studentName, message });
+  console.log("sendMessage: received form data ->", req.body);
 
-  if (!parentName || !studentName || !message) {
-    // Missing some field
-    return res.status(400).send('All fields are required');
-  }
-
-  try {
-    const sql = 'INSERT INTO messages (parent_name, student_name, message) VALUES (?, ?, ?)';
-    await db.execute(sql, [parentName, studentName, message]);
-    console.log('sendMessage: insert successful');
+  const sql = 'INSERT INTO messages (parent_name, student_name, message) VALUES (?, ?, ?)';
+  db.query(sql, [parentName, studentName, message], (err, result) => {
+    if (err) {
+      console.error("sendMessage: DB error ->", err);
+      return res.status(500).send("Internal Server Error: " + err.message);
+    }
+    console.log("sendMessage: message inserted, result ->", result);
     res.redirect('/teacherDashboard');
-  } catch (err) {
-    console.error('sendMessage: DB error ->', err);
-    res.status(500).send('Internal Server Error: ' + err.message);
-  }
-};
-
-exports.getMessagesByParent = async (req, res) => {
-  const { parentName } = req.params;
-  console.log('getMessagesByParent: for parent ->', parentName);
-
-  if (!parentName) {
-    return res.status(400).send('Parent name required');
-  }
-
-  try {
-    const sql = 'SELECT * FROM messages WHERE parent_name = ? ORDER BY timestamp DESC';
-    const [rows] = await db.execute(sql, [parentName]);
-    console.log('getMessagesByParent: fetched rows ->', rows.length);
-    res.render('viewmessages', { parentName, messages: rows });
-  } catch (err) {
-    console.error('getMessagesByParent: DB error ->', err);
-    res.status(500).send('Internal Server Error: ' + err.message);
-  }
+  });
 };
